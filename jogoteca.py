6c19e897d16b144162d15409c691585d81b66a2d
Jorge <jorge.wellison@dce.ufpb.br>
2019-09-02 20:53:16 -0300

    demoreemorei a commitar lol kk template herdado e jogos sendo adicionados à lista

---
+++ jogoteca.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect #importando do pacote flask a classe Flask
+
+app = Flask(__name__) #app recebe o objeto instanciado, que executa o modulo __name__
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon GO', 'RPG', 'Mobile')
+jogo3 = Jogo('Clash Royale', 'Estratégia', 'Mobile')
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/') #diretiva mostrando o caminho da rota
+def index():
+    return render_template('lista.html', titulo='Jogos',
+                           jogos = lista) #helper ajuda a renderizar o html
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+     nome = request.form['nome']
+     categoria = request.form['categoria']
+     console = request.form['console']
+     jogo = Jogo(nome, categoria, console)
+     lista.append(jogo)
+     return redirect('/')
+
+app.run(debug=True) #rodando o app
